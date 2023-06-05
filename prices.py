62cf1a9af904b57f2de71bac626a06a7854b4d24
hvosdt <hvosdt@gmail.com>
2023-06-05 11:41:36 +0300

    fix

---
+++ prices.py
@@ -0,0 +1,11 @@
+from aiogram import types
+
+MARAPHON = types.LabeledPrice(
+    label = 'Марафон Больше чем селфи',
+    amount = 590*100
+)
+
+VPN = types.LabeledPrice(
+    label = 'Подписка на VPN (1 месяц)',
+    amount = 200*100
+)
